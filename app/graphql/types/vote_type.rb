ccd156b713f7f0a4dee7a71fe5e54fb324bd2674
shon0 <shono.koga@gmail.com>
2022-05-16 13:47:52 +0000

    add relationship of user,link,vote

--- app/graphql/types/vote_type.rb
+++ app/graphql/types/vote_type.rb
@@ -3,7 +3,7 @@
 module Types
   class VoteType < Types::BaseObject
     field :id, ID, null: false
-    field :user, Types::UserType, null: false
-    field :link, Types::LinkType, null: false
+    field :user, UserType, null: false
+    field :link, LinkType, null: false
   end
 end
